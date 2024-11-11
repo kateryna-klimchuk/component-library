13f04ab3c817dec7445c3b01a6919342c1aa5a2f
Kateryna Klimchuk <ketyclima@gmail.com>
2024-11-11 20:27:03 +0100

    Updates

---
+++ assets/Button.stories-bx9q-gvc.js
@@ -0,0 +1,20 @@
+import{B as b}from"./Button-DWzwTCIs.js";import"./jsx-runtime-DFKZ3ixi.js";import"./index-SSXOyoI7.js";const x={title:"Example/Button",component:b},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}};var o,t,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: "Button"
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var c,l,m;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: "Button"
+  }
+}`,...(m=(l=a.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var p,u,i;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    label: "Button"
+  }
+}`,...(i=(u=e.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var d,g,B;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    label: "Button"
+  }
+}`,...(B=(g=s.parameters)==null?void 0:g.docs)==null?void 0:B.source}}};const f=["Primary","Secondary","Large","Small"];export{e as Large,r as Primary,a as Secondary,s as Small,f as __namedExportsOrder,x as default};
---
+++ assets/Header.stories-TuPmUI02.js
@@ -0,0 +1,7 @@
+import{H as c}from"./Header-D2wPuE7M.js";import"./jsx-runtime-DFKZ3ixi.js";import"./index-SSXOyoI7.js";import"./Button-DWzwTCIs.js";const i={title:"Example/Header",component:c},e={args:{user:{name:"Jane Doe"}}},r={};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: "Jane Doe"
+    }
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var t,n,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const l=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,l as __namedExportsOrder,i as default};
