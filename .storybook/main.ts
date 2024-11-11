9570175d0ec24e7acdcb59c1c43e27fa16b91a10
Kateryna Klimchuk <ketyclima@gmail.com>
2024-11-11 19:41:55 +0100

    Revert changes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,18 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/preset-create-react-app",
-    // "@storybook/addon-onboarding",
-    // "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    // "@storybook/addon-interactions",
-    // "@storybook/addon-mdx-gfm",
+    "@storybook/addon-interactions",
   ],
-
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-
   staticDirs: ["../public"],
-
-  docs: {
-    autodocs: true,
-  },
 };
 export default config;
--- storybook-static/assets/Header.stories-D5vX407f.js
+++
@@ -1,7 +0,0 @@
-import{H as c}from"./Header-Df8eUBDA.js";import"./Button-DifjoY9e.js";import"./index-DAFOQFY5.js";const u={title:"Example/Header",component:c},e={args:{user:{name:"Jane Doe"}}},r={};var a,o,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: "Jane Doe"
-    }
-  }
-}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var t,n,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const i=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,i as __namedExportsOrder,u as default};
