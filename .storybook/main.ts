b68cacbb951444ad626d0918cbf4d6859df6d7aa
Kateryna Klimchuk <ketyclima@gmail.com>
2024-11-11 19:19:11 +0100

    Add dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,21 +2,25 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    // "@storybook/addon-onboarding",
+    // "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    // "@storybook/addon-interactions",
+    // "@storybook/addon-mdx-gfm",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
   staticDirs: ["../public"],
+
+  docs: {
+    autodocs: true,
+  },
 };
 export default config;
---
+++ storybook-static/assets/Button.stories-LucUok_W.js
@@ -0,0 +1,20 @@
+import{B as b}from"./Button-DifjoY9e.js";import"./index-DAFOQFY5.js";const z={title:"Example/Button",component:b},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}};var o,t,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: "Button"
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var c,l,m;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: "Button"
+  }
+}`,...(m=(l=a.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var u,p,i;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    label: "Button"
+  }
+}`,...(i=(p=e.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var d,g,B;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    label: "Button"
+  }
+}`,...(B=(g=s.parameters)==null?void 0:g.docs)==null?void 0:B.source}}};const x=["Primary","Secondary","Large","Small"];export{e as Large,r as Primary,a as Secondary,s as Small,x as __namedExportsOrder,z as default};
