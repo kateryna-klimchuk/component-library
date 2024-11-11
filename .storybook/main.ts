9b6f017b346b1b04c2d00c442d13ebf5dfb9ceda
Kateryna Klimchuk <ketyclima@gmail.com>
2024-11-11 22:47:45 +0100

    Fixes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,9 +2,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  core: {
+    builder: "@storybook/builder-vite",
+  },
 
   addons: [
-    "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
