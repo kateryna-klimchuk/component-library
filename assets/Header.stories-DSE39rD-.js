298ece4390889b923361513a0a39b164c49955db
Kateryna Klimchuk <ketyclima@gmail.com>
2024-11-11 22:13:19 +0100

    Updates

--- assets/Header.stories-DSE39rD-.js
+++ assets/Header.stories-DSE39rD-.js
@@ -1,4 +1,4 @@
-import{H as c}from"./Header-D2wPuE7M.js";import"./jsx-runtime-DFKZ3ixi.js";import"./index-SSXOyoI7.js";import"./Button-DWzwTCIs.js";const i={title:"Example/Header",component:c},e={args:{user:{name:"Jane Doe"}}},r={};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+import{H as c}from"./Header-C0ZAGD5M.js";import"./jsx-runtime-DFKZ3ixi.js";import"./index-SSXOyoI7.js";import"./Button-C1haYeAP.js";const i={title:"Example/Header",component:c},e={args:{user:{name:"Jane Doe"}}},r={};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
   args: {
     user: {
       name: "Jane Doe"
